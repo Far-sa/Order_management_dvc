f36da00cc2c73962839239d134b218d89d5f1ad9
farsa <puppetcooder@gmail.com>
2024-06-04 11:07:04 +0300

    branch main/optimize both orders/gateway svcs main logic code

--- gateway/main.go
+++ gateway/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
 
 	common "github.com/Far-sa/commons"
+	"github.com/Far-sa/commons/discovery"
 	"github.com/Far-sa/commons/discovery/consul"
 	"github.com/Far-sa/gateway/gateway"
 	"github.com/Far-sa/gateway/handler"
@@ -19,15 +22,33 @@ var (
 )
 
 func main() {
+
 	registry, err := consul.NewRegistry(consulAddr, serviceName)
 	if err != nil {
 		panic(err)
 	}
 
-	gateway.NewGRPCGateway(registry)
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, httpAddr); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			if err := registry.HealthCheck(instanceID, serviceName); err != nil {
+				log.Fatal("failed to health check")
+			}
+			time.Sleep(time.Second * 1)
+		}
+	}()
+
+	defer registry.Unregister(ctx, instanceID, serviceName)
+
+	orderGateway := gateway.NewGRPCGateway(registry)
 
 	mux := http.NewServeMux()
-	handler := handler.New()
+	handler := handler.New(orderGateway)
 	handler.RegisterRoutes(mux)
 
 	log.Printf("starting HTTP server on %s", httpAddr)
--- orders/main.go
+++ orders/main.go
@@ -4,27 +4,54 @@ import (
 	"context"
 	"log"
 	"net"
+	"time"
 
 	common "github.com/Far-sa/commons"
+	"github.com/Far-sa/commons/discovery"
+	"github.com/Far-sa/commons/discovery/consul"
 	"github.com/Far-sa/order/handler"
 	"github.com/Far-sa/order/repository"
 	"github.com/Far-sa/order/service"
+
+	_ "github.com/joho/godotenv/autoload"
 	"google.golang.org/grpc"
 )
 
 var (
-	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:2000")
+	serviceName = "orders"
+	grpcAddr    = common.EnvString("GRPC_ADDR", "localhost:2000")
+	consulAddr  = common.EnvString("CONSUL_ADDR", "localhost:8500")
 )
 
 func main() {
+	registry, err := consul.NewRegistry(consulAddr, serviceName)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, grpcAddr); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			if err := registry.HealthCheck(instanceID, serviceName); err != nil {
+				log.Fatal("failed to health check")
+			}
+			time.Sleep(time.Second * 1)
+		}
+	}()
+
+	defer registry.Unregister(ctx, instanceID, serviceName)
+
+	grpcServer := grpc.NewServer()
 
 	lis, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
-		log.Fatalf("error listening : %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
-	defer lis.Close()
-
-	grpcServer := grpc.NewServer()
 
 	repo := repository.New()
 	svc := service.New(repo)
